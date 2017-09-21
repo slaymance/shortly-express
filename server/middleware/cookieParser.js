cc97e6713feda25d3776f23c2c7aa6ec92f8a52b
Shane Laymance <shane.laymance@gmail.com>
2017-09-20 19:15:35 -0700

    get and parse cookies, assign cookies to request

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,8 +1,17 @@
+const querystring = require('querystring');
+const _ = require('lodash');
+
 const parseCookies = (req, res, next) => {
   // access the cookie ??
-  if (req.headers.Cookies === undefined) {
+  if (req.headers.cookie === undefined) {
     req.cookies = {};
     
+  } else {
+    let unparsedArray = (req.headers.cookie).split('; ');
+    req.cookies = unparsedArray.reduce((acc, val) => {
+      let parsedCookie = querystring.parse(val);
+      return _.extend(acc, parsedCookie);
+    }, {});
   }
   next();
 };
